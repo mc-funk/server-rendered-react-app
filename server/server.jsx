4edaa8b008d0d17d261203e14de4b383f7faab4e
Michelle C. Funk <michelle-funk@pluralsight.com>
2020-08-27 10:06:42 -0500

    general cleanup

--- server/server.jsx
+++ server/server.jsx
@@ -11,61 +11,39 @@ import { App } from '../client/App';
 const app = new express();
 const port = 7777;
 
-// app.use(compression());
 app.use(express.static('dist'));
 
+// TODO: implement a test API call
 // app.get("/data", async (_req, res) => {
 
 //     res.json(await getData());
 
 // });
 
-// app.get("/vote/:answerId", (req, res) => {
-
-//     const { query, params } = req;
-//     modifyAnswerUpvotes(params.answerId, parseInt(query.increment));
-
-// });
-
 app.get('/', async (_req, res) => {
-  // const { questions, answers } = await getData();
-  const data = { name: 'bob' };
+  const data = { name: 'bb' };
   const { html, css, ids } = renderStatic(() =>
     renderToString(<App {...data} />)
   );
   // renderToStaticMarkup may be better as it removes react-specific markup that we would not need for static rendering
 
-  //   console.log("css, ids", css, ids);
   const index = readFileSync(`public/index.html`, `utf8`);
   const browser = await puppeteer.launch();
 
   try {
     const page = await browser.newPage();
-    /** <script
-  crossorigin
-  src="https://unpkg.com/react@16/umd/react.development.js"
-></script>
-<script src="client.js"></script> */
-    await page.addScriptTag({
-      url: 'https://unpkg.com/react@16/umd/react.development.js',
-    });
-    console.log(`Current directory: ${process.cwd()}`);
-    // await page.addScriptTag({ url: "/dist/client.js" });
-    // await page.setContent(html);
-    const style = await page.evaluate(
+
+    // await page.addScriptTag({
+    //   url: 'https://unpkg.com/react@16/umd/react.development.js',
+    // });
+
+    await page.evaluate(
       (index, html, css) => {
         const head = document.createElement('head');
         const style = document.createElement('style');
         style.type = 'text/css';
         style.appendChild(document.createTextNode(css));
         document.head.append(style);
-
-        // Create our shared stylesheet:
-        const sheet = new CSSStyleSheet();
-        sheet.replaceSync(css);
-
-        // Apply the stylesheet to a document:
-        document.adoptedStyleSheets = [sheet];
 
         const body = document.createElement('body');
         body.innerHTML = index.replace('{{rendered}}', html);
@@ -75,15 +53,10 @@ app.get('/', async (_req, res) => {
       html,
       css
     );
-    console.log('style', style);
-    // // Remove scripts and html imports. They've already executed.
-    // await page.evaluate(() => {
-    // 	const elements = document.querySelectorAll('script, link[rel="import"]');
-    // 	elements.forEach(e => e.remove());
-    // });
+
     await page.screenshot({ path: 'public/screenshot.png' });
     const puppeteerHTML = await page.content();
-    console.log('puppeteerHTML', puppeteerHTML);
+
     // Close the page we opened here (not the browser).
     await page.close();
     res.send(puppeteerHTML);
